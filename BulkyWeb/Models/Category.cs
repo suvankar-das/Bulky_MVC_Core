59eea06127a6604d7d963c8ea5c4ed615d31c2c5
Suvankar Das <hi.suvankar@gmail.com>
2024-02-14 14:21:22 +0530

    adding connection string to app

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // all the properties that we define , should be the column of that table named category
+
+        /* By default, if the name is purely ID,entity frameowrk 
+         core will automatically think that this is the primary key for this table.
+        But You should always use Key data annotation if the property name is other than Id, lets say ctaegory__id
+
+        So If property name is Id or CategoryId , then EF will think as primary key.
+        */
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        // With Required data annotation , in table this column should have not null 
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,21 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Tell application that , we want to use EF
+/* We are telling that the DB context that we have in our project will be using SQL Server.
+ using this line options => options.UseSqlServer() 
+
+DefaultConnection is the name of connection string that is stored inside appsettings.json file
+*/
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
